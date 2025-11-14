c0115903bd2d76143303ee55f3cc1f30abe802d8
Fraol-Gmichael <Fraol.Gmichael@gmail.com>
2025-11-14 10:39:17 +0300

    Initialize project

---
+++ src/main/java/com/fraolgmichael/promoqouter/PromoqouterApplication.java
@@ -0,0 +1,13 @@
+package com.fraolgmichael.promoqouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoqouterApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(PromoqouterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fraolgmichael/promoqouter/PromoqouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraolgmichael.promoqouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoqouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
