7f0d6fdd0837980a8daf640dc4a0622488246536
Fraol-Gmichael <Fraol.Gmichael@gmail.com>
2025-11-14 14:23:03 +0300

    Introduce BaseEntity as a mapped superclass for common entity fields
    Signed-off-by: Fraol-Gmichael <Fraol.Gmichael@gmail.com>

---
+++ src/main/java/com/fraolgmichael/promoqouter/common/dataaccess/BaseEntity.java
@@ -0,0 +1,47 @@
+package com.fraolgmichael.promoqouter.common.dataaccess;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    protected UUID id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
