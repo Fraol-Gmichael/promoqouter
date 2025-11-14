f0d78442b6ac8e93d42637f215a36ff0aa2aa671
Fraol-Gmichael <Fraol.Gmichael@gmail.com>
2025-11-14 15:25:41 +0300

    Extend `ResponseCodes` with NOT_FOUND and ALREADY_EXISTS response codes
    Signed-off-by: Fraol-Gmichael <Fraol.Gmichael@gmail.com>

--- src/main/java/com/fraolgmichael/promoqouter/common/exception/ResponseCodes.java
+++ src/main/java/com/fraolgmichael/promoqouter/common/exception/ResponseCodes.java
@@ -3,6 +3,10 @@ package com.fraolgmichael.promoqouter.common.exception;
 public class ResponseCodes {
     public static final ResponseCode BAD_REQUEST =
             ResponseCode.builder().code(400).internalCode("400-0").message("Bad request").build();
+    public static final ResponseCode NOT_FOUND =
+            ResponseCode.builder().code(404).internalCode("404-0").message("Not found").build();
+    public static final ResponseCode ALREADY_EXISTS =
+            ResponseCode.builder().code(409).internalCode("409-0").message("Already exists").build();
 
     private ResponseCodes() {
         // don't initialize me
