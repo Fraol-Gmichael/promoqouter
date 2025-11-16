f65228b6a034a3208c12afb9bd0ec20ed8b66c82
Fraol-Gmichael <Fraol.Gmichael@gmail.com>
2025-11-16 17:02:43 +0300

    Add fields on CartRequestDto
    Signed-off-by: Fraol-Gmichael <Fraol.Gmichael@gmail.com>

--- src/main/java/com/fraolgmichael/promoqouter/promotion/dto/CartRequestDto.java
+++ src/main/java/com/fraolgmichael/promoqouter/promotion/dto/CartRequestDto.java
@@ -1,7 +1,23 @@
 package com.fraolgmichael.promoqouter.promotion.dto;
 
+import com.fraolgmichael.promoqouter.common.service.CustomerSegment;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 
+import java.util.List;
+import java.util.UUID;
+
 @Builder(toBuilder = true)
-public record CartRequestDto() {
+public record CartRequestDto(@NotEmpty(message = "at least one item is required") List<@Valid CartItem> items,
+                             CustomerSegment customerSegment,
+                             List<@Valid @NotEmpty(message = "promoCode is required") String> promoCodes) {
+
+    public record CartItem(
+            @NotNull(message = "productId is required") UUID productId,
+            @NotNull(message = "qty is required") @Positive(message = "qty must be positive number") Long qty
+    ) {
+    }
 }
